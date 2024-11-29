9db26773ab93e7fa1d905e55ac78858ee43a7c1d
Leonhard Reiter <117082106+leonhardreitr@users.noreply.github.com>
2024-11-29 10:40:11 +0100

    Gut Morning

--- code/intro_mlm.R
+++ code/intro_mlm.R
@@ -3,6 +3,7 @@
 # load some libraries
 
 library(tidyverse)
+library(parameters)
 library(modelbased)
 library(lme4)
 library(afex)
@@ -29,9 +30,28 @@ d_agg <-
     .by = c(PID, modality)
   )
 
+library(ez)
+
+ezANOVA(data = d_agg, dv = RT, wid = PID, within = modality) |> summary()
+
+
 d$modality <- ifelse(d$modality == "Audio-only", 0, 1)
 
 
-mod <- lmer(
-  
-)
+mod <- lmer(RT ~ 1 + modality + 
+         (1 + modality|PID) + (1 + modality|stim), 
+       data = d)
+
+
+
+parameters::model_parameters(mod) -> pra
+library(see)
+plot(pra) +  theme(panel.background = element_blank(),         
+                   panel.grid.major = element_blank(),
+                   panel.grid.minor = element_blank(),
+                   panel.border = element_rect(colour = "black", fill = NA),
+                   legend.position = "none",
+                   axis.text = element_text(size = 14), 
+                   axis.title = element_text(size = 14))
+modelbased::estimate_grouplevel(mod)
+estimate_prediction(mod)
